db632bfba0df134f3005fa001239e2390d7ede11
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-03-19 18:28:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your code here
+function majority(){
+	let max=0, count=0;
+	for(let i=0;i<nums.length;i++){
+		if(count==0) max=nums[i];
+		if(nums[i]==max) count++;
+		else count--;
+	}
+	console.log(max);
+}
+majority();
